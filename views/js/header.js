6e9969f95f7bc9a37f8a640b70718c024c4b78e1
Hoàng Bình Minh <111293086+hbm1812@users.noreply.github.com>
2023-10-02 15:31:39 +0700

    Update header
    - update "menu list"

--- views/js/header.js
+++ views/js/header.js
@@ -1,9 +1,11 @@
-const showBtn = document.querySelector('#adress-hover')
-const hideBtn = document.querySelector('#hide')
-const div = document.querySelector('header-content-holder')
-showBtn.addEventListener('click', () => {
-  div.style.display = 'block'
-})
-hideBtn.addEventListener('click', () => {
-  div.style.display = 'none'
-})
+
+// function callHoverEvent(){
+//   var element = document.getElementById("menu1")
+//   element.style.display = 'block';
+// }
+// function outHoverEvent(){
+//   var element = document.getElementById("menu1")
+//   element.style.display = 'none';
+// }
+
+
